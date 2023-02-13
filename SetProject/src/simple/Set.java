7bf2d38769384e625badcf2f3af5faf15c5c3d70
mandadichaitanya1996 <mandadi.chaitanya1@gmail.com>
2023-02-13 09:54:39 +0100

    SpringTestComit

---
+++ SetProject/src/simple/Set.java
@@ -0,0 +1,20 @@
+package simple;
+
+ 
+
+public class Set {
+	
+	public void meth(int a , int b)
+	{
+		System.out.println(a-b);
+	}
+
+
+	public static void main(String[] args)
+	{
+		 Set s = new Set();
+		 s.meth(2, 0);
+	
+	
+}
+}
